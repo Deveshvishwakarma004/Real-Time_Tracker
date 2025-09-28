e3db8d77d6cbc147b0d393fa29223c1d1ad8593a
Devesh Vishwakarma <deveshv0412@gmail.com>
2025-09-28 13:51:39 +0530

    Initial commit

---
+++ public/js/script.js
@@ -0,0 +1,41 @@
+const socket = io();
+
+if (navigator.geolocation) {
+  navigator.geolocation.watchPosition(
+    (position) => {
+      const { latitude, longitude } = position.coords;
+      console.log("My GPS Location:", latitude, longitude);
+      socket.emit("send-location", { latitude, longitude });
+    },
+    (error) => {
+      console.error(error);
+    },
+    {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    }
+  );
+}
+const map = L.map("map").setView([0, 0], 15);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+const markers = {};
+socket.on("receive-location", (data) => {
+  const { id, latitude, longitude } = data;
+  map.setView([latitude, longitude]);
+  if (markers[id]) {
+    markers[id].setLatLng([latitude, longitude]);
+  } else {
+    markers[id] = L.marker([latitude, longitude]).addTo(map);
+  }
+});
+
+
+socket.on("user-diconnected", (id)=>{
+    if(markers[id]){
+        map.removeLayer(markers[id]);
+        delete markers[id]
+    }
+})
